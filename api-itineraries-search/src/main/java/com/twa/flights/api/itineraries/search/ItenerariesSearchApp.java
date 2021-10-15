99deb95b99d31a255be54f6ce4e7494b3426eff0
Mohit Kumar <mkumar@tekion.com>
2021-10-15 22:16:08 +0530

    p1m1: Simple API Test

--- api-itineraries-search/src/main/java/com/twa/flights/api/itineraries/search/ItenerariesSearchApp.java
+++ api-itineraries-search/src/main/java/com/twa/flights/api/itineraries/search/ItenerariesSearchApp.java
@@ -1,15 +1,21 @@
-package com.twa.flights.api.provider.alpha;
+package com.twa.flights.api.itineraries.search;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.twa.flights.api" })
-public class App implements WebMvcConfigurer {
+public class ItenerariesSearchApp implements WebMvcConfigurer {
 
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        run(args);
     }
+
+    public static ConfigurableApplicationContext run(String[] args) {
+        return SpringApplication.run(ItenerariesSearchApp.class, args);
+    }
+
 }
